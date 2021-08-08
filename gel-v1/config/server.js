bc0e9671a7a8d48dc4cf6d4cfd1cee17298a85b4
Chris Waters <cp-waters@hotmail.co.uk>
2021-08-08 15:52:33 +0100

    Initial creation

---
+++ gel-v1/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2e2284532d4d400a4fa016d1fbe8126'),
+    },
+  },
+});
---
+++ gel-v1/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a57e09e2-1fc8-40f7-acd9-04bcc09c9b28'
+};
